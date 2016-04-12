5bce0e2356433600c31634b1861f0de03e87a09e
PabloGaymardo <paul_gaymard@hotmail.fr>
2016-04-12 11:08:03 +0200

    voila les biatch

--- ESCRIM 2/src/application/Main.java
+++ ESCRIM 2/src/application/Main.java
@@ -5,32 +5,15 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 import metier.Colis;
 import metier.Dimension;
 import partieMission.GrpColis;
 import view.AfficheurGrp;
 
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class Main {
 	
 	public static void main(String[] args) {
-		//launch(args);
 		exempleAfficheurGrp();
 		
 	}
