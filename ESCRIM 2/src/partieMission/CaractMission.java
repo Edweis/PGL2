cda09992304b4bf9c24aeb1656a63b073fcc018f
Paul Galloy <kalakof_92@hotmail.fr>
2016-04-20 18:16:36 +0200

    caractmission

---
+++ ESCRIM 2/src/partieMission/CaractMission.java
@@ -0,0 +1,46 @@
+package partieMission;
+
+import java.util.Date;
+
+public class CaractMission {
+	Date dateDebut;
+	Date dateFin;
+	String lieu;
+	
+	public CaractMission(Date dateDebut, Date dateFin, String lieu){
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.lieu = lieu;
+	}
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public String getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+	
+	
+	
+	
+	
+	
+
+}
